402cc15b6045217e50354323b4749335f8f491ee
Antonio <amcm23597@gmail.com>
2019-09-29 16:28:05 +0200

    commit for fran xd

---
+++ src/API/prueba.go
@@ -0,0 +1,138 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
+)
+
+type Post struct {
+	ID     string `json:"id"`
+	Nombre string `json:"nombre"`
+	Apellidos string `json:"apellido"`
+}
+
+var db *sql.DB
+var err error
+
+func main() {
+	db, err = sql.Open("mysql", "falor_fralg:fralg100@gmail.com@tcp(falorente.salesianas.es)/falorente_react")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer db.Close()
+	router := mux.NewRouter()
+	router.HandleFunc("/posts", getPosts).Methods("GET")
+	router.HandleFunc("/posts", createPost).Methods("POST")
+	router.HandleFunc("/posts/{id}", getPost).Methods("GET")
+	router.HandleFunc("/posts/{id}", updatePost).Methods("PUT")
+	router.HandleFunc("/posts/{id}", deletePost).Methods("DELETE")
+	http.ListenAndServe(":8000", router)
+}
+
+func getPosts(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+	w.Header().Set("Content-Type", "application/json")
+	var posts []Post
+	result, err := db.Query("SELECT * from clientes")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer result.Close()
+	for result.Next() {
+		var post Post
+		err := result.Scan(&post.ID, &post.Nombre, &post.Apellidos)
+		if err != nil {
+			panic(err.Error())
+		}
+		posts = append(posts, post)
+	}
+	json.NewEncoder(w).Encode(posts)
+}
+
+func setupResponse(w *http.ResponseWriter, req *http.Request) {
+	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+    (*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+    (*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+}
+
+func createPost(w http.ResponseWriter, r *http.Request) {
+	setupResponse(&w, r)
+	if (*r).Method == "OPTIONS" {
+		return
+	}
+	stmt, err := db.Prepare("INSERT INTO clientes(nombre,apellido) VALUES(?,?)")
+	if err != nil {
+		panic(err.Error())
+	}
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		panic(err.Error())
+	}
+	keyVal := make(map[string]string)
+	json.Unmarshal(body, &keyVal)
+	title := keyVal["nombre"]
+	apellido := keyVal["apellido"]
+	_, err = stmt.Exec(title, apellido)
+	if err != nil {
+		panic(err.Error())
+	}
+	fmt.Fprintf(w, "New post was created")
+}
+
+func getPost(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	result, err := db.Query("SELECT id, nombre FROM clientes WHERE id = ?", params["id"])
+	if err != nil {
+		panic(err.Error())
+	}
+	defer result.Close()
+	var post Post
+	for result.Next() {
+		err := result.Scan(&post.ID, &post.Nombre)
+		if err != nil {
+			panic(err.Error())
+		}
+	}
+	json.NewEncoder(w).Encode(post)
+}
+
+func deletePost(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	stmt, err := db.Prepare("DELETE FROM clientes WHERE id = ?")
+	if err != nil {
+		panic(err.Error())
+	}
+	_, err = stmt.Exec(params["id"])
+	if err != nil {
+		panic(err.Error())
+	}
+	fmt.Fprintf(w, "Post with ID = %s was deleted", params["id"])
+}
+
+func updatePost(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	stmt, err := db.Prepare("UPDATE clientes SET nombre = ? WHERE id = ?")
+	if err != nil {
+		panic(err.Error())
+	}
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		panic(err.Error())
+	}
+	keyVal := make(map[string]string)
+	json.Unmarshal(body, &keyVal)
+	newTitle := keyVal["nombre"]
+	_, err = stmt.Exec(newTitle, params["id"])
+	if err != nil {
+		panic(err.Error())
+	}
+	fmt.Fprintf(w, "Post with ID = %s was updated", params["id"])
+}
--- src/App.js
+++ src/App.js
@@ -1,26 +1,103 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import M from 'materialize-css';
+import { Navbar, NavItem, TextInput, Table } from 'react-materialize'
+import Button from 'react-materialize/lib/Button';
+import axios from 'axios'
+// ref can only be used on class components
+export default function App() {
+  // get a reference to the element after the component has mounted
+  useEffect(
+    () => {
+      M.AutoInit();
+    }
+  )
 
-function App() {
+  useEffect(() => {
+    axios.get('http://localhost:8000/posts')
+      .then(response => {
+        console.log(response.data);
+        setData(response.data)
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, [])
+
+  const [name, setName] = useState("")
+  const [surname, setSurname] = useState("")
+  const [data, setData] = useState()
+
+  function handleNameChange(e) {
+    setName(e.target.value)
+  }
+  function handleSurnameChange(e) {
+    setSurname(e.target.value)
+  }
+
+  function handleSubmit() {
+    const values = {
+      "nombre": name,
+      "apellidos": surname,
+    }
+
+    axios.post('http://localhost:8000/posts', values)
+      .then(response => {
+        console.log(response.data);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div>
+      <Navbar brand={<a>PruebaMaterialize</a>} alignLinks="right">
+        <NavItem href="">
+          Getting started
+          </NavItem>
+        <NavItem href="components.html">
+          Components
+          </NavItem>
+      </Navbar>
+      <div style={{ padding: "2rem" }}>
+        <h1>Ejemplo de formulario</h1>
+        <TextInput label="Nombre" value={name} onChange={handleNameChange} />
 
-export default App;
+        <TextInput label="Apellidos" value={surname} onChange={handleSurnameChange} />
+
+        <Button onClick={handleSubmit}>Registrar</Button>
+        <hr style={{ marginTop: "3rem" }} />
+        <Table style={{ marginTop: "1rem" }}>
+          <thead>
+            <tr>
+              <th data-field="id">
+                ID
+              </th>
+              <th data-field="name">
+                Nombre
+              </th>
+              <th data-field="surname">
+                Apellidos
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {data && data.map((user, i) => (
+              <tr key={i}>
+                <td>
+                  {user.id}
+                </td>
+                <td>
+                  {user.nombre}
+                </td>
+                <td>
+                  {user.apellido}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      </div>
+    </div>
+  )
+}
